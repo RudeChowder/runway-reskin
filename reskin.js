717f517734aa47d2eb36aae3af134c6b77b42c2f
Stephen Crane <scrane88@gmail.com>
2023-02-16 12:10:46 -0500

    Set up card coloring

---
+++ reskin.js
@@ -0,0 +1,29 @@
+const cards = Array.from(document.querySelectorAll(".backlog-item-card"))
+
+const assigned = (card) => {
+  const assignees = card.querySelector(".backlog-item-card-assignees")
+  return assignees.innerHTML.includes("StephenCrane")
+}
+
+const committed = (card) => {
+  const tagSection = card.querySelector(".backlog-item-card-description-meta")
+  return tagSection.innerHTML.includes("committed")
+}
+
+const blocked = (card) => {
+  const tagSection = card.querySelector(".backlog-item-card-description-meta")
+  return tagSection.innerHTML.includes("blocked")
+}
+
+
+cards.forEach( card => {
+  if (blocked(card)) {
+    card.style.backgroundColor = "#fae7d2"
+  } else if (assigned(card) && committed(card)) {
+    card.style.background = "linear-gradient(-45deg, #CCE7FF 0%, #CCE7FF 50%, #d4ffd9 50%, #d4ffd9 100%)"
+  } else if (assigned(card)) {
+    card.style.backgroundColor = "#d4ffd9"
+  } else if (committed(card)) {
+    card.style.backgroundColor = "#CCE7FF"
+  }
+})
