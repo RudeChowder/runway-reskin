424ee71f3271746d82ac9c01a5dcb89d07ba747d
Stephen Crane <scrane88@gmail.com>
2023-03-09 11:04:01 -0500

    Add event listener

--- reskin.js
+++ reskin.js
@@ -15,15 +15,19 @@ const blocked = (card) => {
   return tagSection.innerHTML.includes("blocked")
 }
 
+const colorize = () => {
+  cards.forEach( card => {
+    if (blocked(card)) {
+      card.style.backgroundColor = "#fae7d2"
+    } else if (assigned(card) && committed(card)) {
+      card.style.background = "linear-gradient(-45deg, #d4ffd9 0%, #d4ffd9 50%, #CCE7FF 50%, #CCE7FF 100%)"
+    } else if (assigned(card)) {
+      card.style.backgroundColor = "#d4ffd9"
+    } else if (committed(card)) {
+      card.style.backgroundColor = "#CCE7FF"
+    }
+  })
+}
 
-cards.forEach( card => {
-  if (blocked(card)) {
-    card.style.backgroundColor = "#fae7d2"
-  } else if (assigned(card) && committed(card)) {
-    card.style.background = "linear-gradient(-45deg, #d4ffd9 0%, #d4ffd9 50%, #CCE7FF 50%, #CCE7FF 100%)"
-  } else if (assigned(card)) {
-    card.style.backgroundColor = "#d4ffd9"
-  } else if (committed(card)) {
-    card.style.backgroundColor = "#CCE7FF"
-  }
-})
+document.addEventListener("change", () => colorize())
+colorize()
